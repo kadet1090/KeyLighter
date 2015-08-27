50161f54eae81941494916abfe079845e8e48afa
Kacper Donat <kadet1090@gmail.com>
2015-08-27 23:34:05 +0200

    Highlighting TokenDump - because why not

---
+++ Language/TokenDump.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Highlighter
+ *
+ * Copyright (C) 2015, Some right reserved.
+ * @author Kacper "Kadet" Donat <kadet1090@gmail.com>
+ * @license http://creativecommons.org/licenses/by-sa/4.0/legalcode CC BY-SA
+ *
+ * Contact with author:
+ * Xmpp: kadet@jid.pl
+ * E-mail: kadet1090@gmail.com
+ *
+ * From Kadet with love.
+ */
+
+namespace Kadet\Highlighter\Language;
+
+
+use Kadet\Highlighter\Matcher\RegexMatcher;
+use Kadet\Highlighter\Parser\Rule;
+
+class TokenDump extends Language
+{
+    public function getRules()
+    {
+        return [
+            'string'  => new Rule(new RegexMatcher("/\x02(.*?)\x03/ms")),
+            'keyword' => new Rule(new RegexMatcher("/(?:^|\\n)\\s*(Start|End) /")),
+            'symbol' => new Rule(new RegexMatcher("/(?:^|\\n)\\s*(?:Start|End) \\((.*?)\\)/")),
+        ];
+    }
+
+    public function getIdentifier()
+    {
+        return 'token-dump';
+    }
+}
