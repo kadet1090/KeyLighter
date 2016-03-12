e4c55f200a1db20d5022b3de9c95c80162df0e6b
Kacper Donat <kadet1090@gmail.com>
2016-03-12 20:05:29 +0100

    Formatter tests

---
+++ Tests/CliFormatterTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Highlighter
+ *
+ * Copyright (C) 2016, Some right reserved.
+ *
+ * @author Kacper "Kadet" Donat <kacper@kadet.net>
+ *
+ * Contact with author:
+ * Xmpp: me@kadet.net
+ * E-mail: contact@kadet.net
+ *
+ * From Kadet with love.
+ */
+
+namespace Kadet\KeyLighter\Tests;
+
+
+use Kadet\Highlighter\Formatter\CliFormatter;
+use Kadet\Highlighter\Parser\Token;
+use Kadet\Highlighter\Parser\TokenIterator;
+use Kadet\Highlighter\Utils\Console;
+
+class CliFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRendering() {
+        $source = 'abc + test';
+        $expected = Console::open(['color' => 'red']).'abc'.Console::close().' '.
+            Console::open(['color' => 'blue']).'+'.Console::close().' '.
+            Console::open(['color' => 'red']).'test'.Console::close().Console::reset();
+
+        $first    = new Token(['token', 'pos' => 0, 'length' => 3]);
+        $operator = new Token(['operator', 'pos' => 4, 'length' => 1]);
+        $second   = new Token(['token.second', 'pos' => 6, 'length' => 4]);
+
+        $iterator = new TokenIterator([
+            $first, $first->getEnd(),
+            $operator, $operator->getEnd(),
+            $second, $second->getEnd()
+        ], $source);
+
+        $formatter = new CliFormatter([
+           'token' => ['color' => 'red'],
+           'operator' => ['color' => 'blue'],
+        ]);
+        $this->assertEquals($expected, $formatter->format($iterator));
+    }
+}
---
+++ Tests/HtmlFormatterTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Highlighter
+ *
+ * Copyright (C) 2016, Some right reserved.
+ *
+ * @author Kacper "Kadet" Donat <kacper@kadet.net>
+ *
+ * Contact with author:
+ * Xmpp: me@kadet.net
+ * E-mail: contact@kadet.net
+ *
+ * From Kadet with love.
+ */
+
+namespace Kadet\KeyLighter\Tests;
+
+
+use Kadet\Highlighter\Formatter\HtmlFormatter;
+use Kadet\Highlighter\Parser\Token;
+use Kadet\Highlighter\Parser\TokenIterator;
+
+class HtmlFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRender() {
+        $source = 'abc + test';
+        $expected = <<<EXPECTED
+<span class="token">abc</span> <span class="operator">+</span> <span class="token second">test</span>
+EXPECTED;
+
+        $first    = new Token(['token', 'pos' => 0, 'length' => 3]);
+        $operator = new Token(['operator', 'pos' => 4, 'length' => 1]);
+        $second   = new Token(['token.second', 'pos' => 6, 'length' => 4]);
+
+        $iterator = new TokenIterator([
+            $first, $first->getEnd(),
+            $operator, $operator->getEnd(),
+            $second, $second->getEnd()
+        ], $source);
+
+        $formatter = new HtmlFormatter();
+        $this->assertEquals($expected, $formatter->format($iterator));
+    }
+}
