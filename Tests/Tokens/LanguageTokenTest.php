b3201a174e6ecbda59bb99bf2596afbe0df7eb9c
Kacper Donat <kadet1090@gmail.com>
2016-04-10 12:10:24 +0200

    LanguageTokenTest

---
+++ Tests/Tokens/LanguageTokenTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Highlighter
+ *
+ * Copyright (C) 2016, Some right reserved.
+ *
+ * @author Kacper "Kadet" Donat <kacper@kadet.net>
+ *
+ * Contact with author:
+ * Xmpp: me@kadet.net
+ * E-mail: contact@kadet.net
+ *
+ * From Kadet with love.
+ */
+
+namespace Kadet\Highlighter\Tests\Tokens;
+
+
+use Kadet\Highlighter\Parser\Token\LanguageToken;
+
+class LanguageTokenTest extends TokenTestCase
+{
+    public function testCreation()
+    {
+        $token = new LanguageToken('name', ['inject' => $this->_language]);
+
+        $this->assertSame($this->_language, $token->inject);
+    }
+}
