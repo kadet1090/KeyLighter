9b01bc71df074584726c2dc664a1e8732ef99bc9
Kacper Donat <kadet1090@gmail.com>
2017-08-15 11:23:03 +0200

    template tags are now treated as keywords in twig

--- Language/Twig.php
+++ Language/Twig.php
@@ -14,12 +14,19 @@ class Twig extends Django
     public function setupRules()
     {
         parent::setupRules();
+
+        $tag = $this->rules->rule('call.template-tag');
+        $this->rules->remove('call.template-tag');
+
         $this->rules->addMany([
             'call' => [
                 new Rule(new RegexMatcher('/(\w+)\s*\(/si'), ['priority' => 1]),
                 'test' => new Rule(new RegexMatcher('/is(?:\s+not)?\s+(\w+)/si'), ['priority' => 1]),
             ],
-            'keyword' => new Rule(new WordMatcher(['only', 'with', 'is', 'not', 'ignore missing', 'in']))
+            'keyword' => [
+                'template-tag' => new Rule(new WordMatcher(['only', 'with', 'is', 'not', 'ignore missing', 'in'])),
+                $tag, // template tags are often used in django as functions, whereas in twig they are more keyword like
+            ]
         ]);
     }
 
