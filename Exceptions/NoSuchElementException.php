6fb22fbde61b643e2095e9e1ff52c930df5e3d3c
Sobak <msobaczewski@gmail.com>
2020-04-12 11:47:44 +0200

    Switch whole project to use strict type checks

--- Exceptions/NoSuchElementException.php
+++ Exceptions/NoSuchElementException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Kadet\Highlighter\Exceptions;
 
 use Symfony\Component\Console\Exception\LogicException;
--- Language/TypeScript.php
+++ Language/TypeScript.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Kadet\Highlighter\Language;
 
 use Kadet\Highlighter\Matcher\RegexMatcher;
