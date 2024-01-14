a4658b8e614383f76de6188d300b46c731bb21e0
Maciej Sobaczewski <msobaczewski@gmail.com>
2024-01-14 03:01:28 +0100

    Upgrade npm dependencies
    This commit upgrades frontend dependencies (for building CSS files) to
    their latest available versions on nodejs 18.x.
    
    These versions no longer rely on Python 2 but require a change in our
    gulpfile instead. We had to explicitly install either sass or node-sass
    package and fix the import there.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp         = require("gulp");
-const sass         = require('gulp-sass');
+const sass         = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("styles", function () {
