6fe693283e782871c5ce777d5c6312b36ac4181f
Maciej Sobaczewski <msobaczewski@gmail.com>
2024-02-05 06:08:35 +0100

    Parameters

--- Language/Batch.php
+++ Language/Batch.php
@@ -61,6 +61,11 @@ class Batch extends GreedyLanguage
             'number'    => new Rule(new RegexMatcher('/(-?(?:0[0-7]+|0[xX][0-9a-fA-F]+|0b[01]+|\d+))/')),
             'delimiter' => new Rule(new RegexMatcher('/^(\$)/m')),
 
+            'symbol.parameter' => new Rule(new RegexMatcher('/(\/[a-z])\b/i'), [
+                'priority' => 0,
+                'context'  => ['!comment'],
+            ]),
+
             'expression' => [
                 new Rule(new RegexMatcher('/(?=(\$\(((?>[^$()]+|(?1))+)\)))/x'), [
                     'context' => Validator::everywhere(),
